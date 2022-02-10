f9a59ca1ed7ed29e93a2f8905808d651e575573d
camronh <camronhaider@gmail.com>
2022-02-10 20:07:42 +0000

    api

---
+++ index.js
@@ -0,0 +1,27 @@
+/*
+
+Build me an API
+
+Libraries to use:
+express nodemon axios
+
+Endpoints
+
+GET - Get a pokemon's stats
+Params: pokemonIdOrName
+Returns: Pokemon Object
+
+
+GET - Get a random pokemon's stats
+Params: --
+Returns: Pokemon Object
+
+
+POST - Battle Pokemon
+Body: {
+    challenger: pokemonIdOrName,
+    opponent: pokemonIdOrName
+}
+Returns: The pokemon object with the higher attack
+
+*/
